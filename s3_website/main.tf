c18bb8e86e2714c079d8c5f25b0a53939ed4ec69
AlkseyD <89952658+AlkseyD@users.noreply.github.com>
2024-05-28 14:45:54 +0300

    Update main.tf

--- s3_website/main.tf
+++ s3_website/main.tf
@@ -1,64 +1,69 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
 }
 
 # Configure AWS provider and creds
-provider "aws" {
-  region		  ="us-east-1"
-  shared_config_files	  =["/home/ec2-user/.aws/config"]
-  shared_credentials_files=["/home/ec2-user/.aws/credentials"]
-}
-
+#provider "aws" {
+#  region                  = "us-east-1"
+#  shared_config_files     = ["/home/ec2-user/.aws/config"]
+#  shared_credentials_files = ["/home/ec2-user/.aws/credentials"]
+#  profile                 = "default"
+#}
 
 # Creating bucket
 resource "aws_s3_bucket" "website" {
   bucket = "terra-bucket-for-lab2dovbah"
-  tags   = {
+
+  tags = {
     Name        = "Website"
     Environment = "Dev"
   }
 }
-resource "aws_s3_bucket_acl" "example_acl" {
-  bucket = aws_s3_bucket.website.id
-  acl    = "public-read"
-}
 
-resource "aws_s3_bucket_website_configuration" "website_config" {
-  bucket = aws_s3_bucket.website.id
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website.bucket
 
- index_document {
+  index_document {
     suffix = "index.html"
   }
+
   error_document {
     key = "error.html"
   }
 }
 
-resource "aws_s3_bucket_policy" "allow_access" {
+resource "aws_s3_bucket_public_access_block" "access_block" {
   bucket = aws_s3_bucket.website.id
-  policy = data.aws_iam_policy_document.allow_access.json
+
+  block_public_acls       = false
+  block_public_policy     = false
+  restrict_public_buckets = false
+  ignore_public_acls      = true
 }
-data "aws_iam_policy_document" "allow_access" {
+
+data "aws_iam_policy_document" "website_read" {
   statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.website.arn}/*"]
+
     principals {
-      type	  = "AWS"
+      type        = "AWS"
       identifiers = ["*"]
     }
-    actions = [
-      "s3:GetObject"
-    ]
-    resources = [
-      aws_s3_bucket.website.arn,
-      "${aws_s3_bucket.website.arn}/*",
-    ]
   }
 }
-resource "aws_s3_object" "indexfile" {
+
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.website.id
+  policy = data.aws_iam_policy_document.website_read.json
+}
+
+resource "aws_s3_bucket_object" "indexfile" {
   bucket       = aws_s3_bucket.website.id
   key          = "index.html"
   source       = "./src/index.html"
@@ -66,5 +71,5 @@ resource "aws_s3_object" "indexfile" {
 }
 
 output "website_endpoint" {
-  value = aws_s3_bucket_website_configuration.website_config.website_endpoint
+  value = aws_s3_bucket.website.bucket_regional_domain_name
 }
