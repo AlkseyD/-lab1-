38e804d9f429cb9f552d91f0b789d942db042102
AlkseyD <89952658+AlkseyD@users.noreply.github.com>
2024-06-10 23:17:24 +0300

    Update main.tf

--- s3_website/main.tf
+++ s3_website/main.tf
@@ -10,7 +10,6 @@ terraform {
 # Configure AWS provider and creds
 provider "aws"{
   region = "us-east-1"
-  #shared_config_files = ["/home/ec2-user/.aws/config"]
   shared_credentials_files = ["/home/ec2-user/.aws/credentials"]
   profile = "default"
 }
